5c8d212fba3925c4b9a7f329146adc0db67b6e34
aligundogdu <ali@vuska.net>
2017-03-31 14:01:56 +0300

    Configure Download Manager.

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 #!/usr/bin/env node
-var version = '0.0.1';
+
+var pjson = require('./package.json');
+var version = pjson.version;
+
 var download, program, exists, path, inquirer, request, chalk;
 var boxes = [];
 var jsonUrl = 'https://raw.githubusercontent.com/Projekod/Projekod-Downloader/master/pkdofile.json';
 
 request = require('request');
 chalk = require('chalk');
-
-/* Check Version */
+inquirer = require('inquirer');
+exists = require('fs').existsSync;
 
 request(jsonUrl, function (error, response, body) {
     if (response.statusCode !== 200) {
@@ -15,7 +18,6 @@ request(jsonUrl, function (error, response, body) {
         console.log(body);
         console.log(response.statusCode);
         console.log(error);
-
         process.exit();
     }
 
@@ -34,9 +36,70 @@ request(jsonUrl, function (error, response, body) {
 });
 
 function main() {
-
+    var choices = boxes.map(function (item, key) {
+        return (key + 1) + '- ' + chalk.yellow(item.name) + ' (' + item.description + ')';
+    });
+    inquirer.prompt([
+        {
+            type: 'list',
+            name: 'download',
+            message: 'Ne vereyim abime ?',
+            paginated: true,
+            choices: choices
+        },
+        {
+            type: 'input',
+            name: 'directory',
+            message: 'Klasör adı ne olsun ? : ',
+            default: function () {
+                return '.';
+            }
+        }
+    ]).then(function (answer) {
+        var downloadStr = answer.download;
+        var keys = downloadStr.split("- ");
+        var directory = answer.directory;
+        console.log(directory);
+        if (keys.length > 1) {
+            var key = (keys[0] - 1);
+            var box = boxes[key];
+            rockNRoll(box, directory);
+        } else {
+            console.log(chalk.red("Cevap metni düzenlenemedi."));
+            process.exit();
+        }
+    });
 }
 
+function rockNRoll(box, directory) {
+    var inHere = !directory || directory === '.';
+
+    if (exists(directory)) {
+        inquirer.prompt([{
+            type: 'confirm',
+            message: inHere
+                ? 'Bulunduğun dizine yüklemek istiyor musun ?'
+                : 'Belirttiğin dizin zaten var onun içine yüklemek istiyor musun ?',
+            name: 'ok'
+        }]).then(function (answer) {
+            if (!answer.ok) {
+                console.log(chalk.red("Kullanıcı isteği ile program sonlandırıldı."));
+                process.exit();
+            }
+            downloadBox(box, directory);
+        })
+    } else {
+        downloadBox(box, directory);
+    }
+}
+
+function downloadBox(box, directory) {
+    download(box.target, directory, function (err) {
+        if (!err) {
+            console.log(chalk.green('İndirme işlemi tamamlandı'));
+        }
+    });
+}
 
 function checkVersion(a, b) {
     if (a === b) {
